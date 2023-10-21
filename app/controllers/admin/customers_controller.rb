2529c0625a4ba86c41ec413eb575f2b0ba27646b
oosawa-shingo <141128249+oosawa-shingo@users.noreply.github.com>
2023-10-21 06:15:20 +0000

    [update]レイアウト完了（ユーザーログイン画面）

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -20,6 +20,6 @@ class Admin::CustomersController < ApplicationController
    private
 
   def customer_params
-    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number)
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :is_active, :email)
   end
 end
